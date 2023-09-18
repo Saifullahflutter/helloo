c33117eb2f38093f6ee61a16596c6d028c715aa1
saifullahflutter <sa.ullah@pk.micromerger.com>
2023-09-18 15:38:13 +0500

    msg 1

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,25 +2,31 @@ import 'package:test/test.dart';
 import 'package:unit_test_counter/counter.dart';
 
 void main() {
-  group('Counter', () {
-    test('value should start at 0', () {
-      expect(Counter().value, 0);
-    });
+  group(
+    'Counter',
+    () {
+      test('value should start at 0', () {
+        expect(Counter().value, 0);
+      });
 
-    test('value should be incremented', () {
-      final counter = Counter();
+      test('value should be incremented', () {
+        final counter = Counter();
 
-      counter.increment();
+        counter.increment();
 
-      expect(counter.value, 1);
-    });
+        expect(counter.value, 1);
+      });
 
-    test('value should be decremented', () {
-      final counter = Counter();
+      test(
+        'value should be decremented',
+        () {
+          final counter = Counter();
 
-      counter.decrement();
+          counter.decrement();
 
-      expect(counter.value, -1);
-    });
-  });
+          expect(counter.value, -1);
+        },
+      );
+    },
+  );
 }
